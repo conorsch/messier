0db29dca4c39df8aa41a0e7c00e646c11d278958
Conor Schaefer <conor.schaefer@gmail.com>
2015-12-28 19:47:05 -0800

    Adds failing test

--- tests/test_messier.py
+++ tests/test_messier.py
@@ -9,6 +9,7 @@ Tests for `messier` module.
 """
 
 import unittest
+import os
 
 from messier import messier
 
@@ -23,6 +24,17 @@ class TestMessier(unittest.TestCase):
 
     def test_000_something(self):
         pass
+
+    def test_init(self):
+        """
+        Create a .messier config file and validate its contents.
+        """
+        messier.config.init()
+        assert os.path.existS(".messier")
+
+
+
+
 
 
 if __name__ == '__main__':
